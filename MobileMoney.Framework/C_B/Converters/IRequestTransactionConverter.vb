9521ef1de29f2eb8cc6d4a6ed76dc7119c2a42ca
Ayubu <ayubumasasi@gmail.com>
2016-05-13 06:28:30 +0300

    Serialize to MpesaRequestTransaction through converter

--- MobileMoney.Framework/C_B/Converters/IRequestTransactionConverter.vb
+++ MobileMoney.Framework/C_B/Converters/IRequestTransactionConverter.vb
@@ -4,7 +4,7 @@ Namespace C_B.Converters
         Inherits Newtonsoft.Json.JsonConverter
 
         Public Overrides Sub WriteJson(writer As JsonWriter, value As Object, serializer As JsonSerializer)
-            Throw New NotImplementedException()
+            serializer.Serialize(writer, value)
         End Sub
 
         Public Overrides Function CanConvert(objectType As Type) As Boolean
@@ -12,7 +12,7 @@ Namespace C_B.Converters
         End Function
 
         Public Overrides Function ReadJson(reader As JsonReader, objectType As Type, existingValue As Object, serializer As JsonSerializer) As Object
-            Throw New NotImplementedException()
+            Return serializer.Deserialize(Of MpesaRequestTransaction)(reader)
         End Function
     End Class
 End Namespace
